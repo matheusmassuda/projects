abf8c875ac9318c48d02aec16cd9da3c1d845f74
Matheus Luiz Massuda <mat.massuda@gmail.com>
2024-11-08 17:53:32 -0300

    Publicação de documentos HTML, CSS e JS

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+function clickMenu() {
+
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+};
